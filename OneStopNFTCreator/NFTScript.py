80f42c59f6a1df394cb61749727cfafa2fdf971c
“Michele” <bonini.michele@yahoo.it>
2023-04-15 21:03:23 +0200

    submission create/transfer nft

--- OneStopNFTCreator/NFTScript.py
+++ OneStopNFTCreator/NFTScript.py
@@ -1,105 +1,66 @@
 import hashlib
 import json
-
+from algosdk import transaction
+import os
 import algosdk
 from algosdk.v2client import algod
 from beaker import sandbox
 
 
 def mintNFT(algod_client, creator_address, creator_private_key, asset_name, asset_unit_name):
-    # Get the creator's account information
-    creator_account_info = algod_client.account_info(creator_address)
-    creator_account_public_key = creator_account_info['address']
-    creator_account_seq_num = creator_account_info['sequence']
-
-    # Create the asset creation transaction
-    params = algod_client.suggested_params()
-    txn = algosdk.future.transaction.AssetConfigTxn(
-        sender=creator_account_public_key,
-        sp=params,
-        total=1,  # Only one of this NFT will be created
-        default_frozen=False,  # Allow the asset to be transferred
+    sp = algod_client.suggested_params()
+    txn = transaction.AssetConfigTxn(
+        sender=creator_address,
+        sp=sp,
+        default_frozen=False,
         unit_name=asset_unit_name,
         asset_name=asset_name,
-        manager=creator_address,  # Creator has all permissions
-        reserve=None,
-        freeze=None,
-        clawback=None,
-        url=None,
-        metadata_hash=None,
+        manager=creator_address,
+        reserve=creator_address,
+        freeze=creator_address,
+        clawback=creator_address,
+        url="ipfs://QmUZ2u6PExmzRCy3gajq9NrxW2ZvpULinuTQvYAEfSewZu#arc3", #the 32 bytes of the SHA-256 digest of the above JSON file
+        metadata_hash=b'\n@.\xcag\x16\xd68%\xa7<)\xc0\xcc\xedt\x88\xd2j\x1f\xa1\x9d\xc2nY\xd5\x9e\xc4$?\x18\xa7',
+        total=1,
+        decimals=0,
     )
 
-    # Sign the transaction with the creator's private key
-    signed_txn = txn.sign(creator_private_key)
-
-    # Send the transaction
-    algod_client.send_transaction(signed_txn)
-
+    stxn = txn.sign(creator_private_key)
+    # Send the transaction to the network and retrieve the txid.
+    txid = algod_client.send_transaction(stxn)
     # Wait for the transaction to be confirmed
-    confirmed_txn = None
-    while confirmed_txn is None:
-        try:
-            txn_id = signed_txn.transaction.get_txid()
-            confirmed_txn = algod_client.pending_transaction_info(txn_id)
-        except Exception:
-            pass
+    results = transaction.wait_for_confirmation(algod_client, txid, 4)
 
-    # Get the asset ID from the confirmed transaction
-    asset_id = confirmed_txn['asset-config-transaction']['asset-id']
+    # grab the asset id for the asset we just created
+    created_asset = results["asset-index"]
+    print(f"Asset ID created: {created_asset}")
 
-    return asset_id
-
-    return 0  #your confirmed transaction's asset id should be returned instead
+    return created_asset
 
 
 def transferNFT(algod_client, creator_address, creator_private_key, receiver_address, receiver_private_key, asset_id):
-    # Get the creator's account information
-    creator_account_info = algod_client.account_info(creator_address)
-    creator_account_public_key = creator_account_info['address']
-    creator_account_seq_num = creator_account_info['sequence']
-
-    # Get the receiver's account information
-    receiver_account_info = algod_client.account_info(receiver_address)
-    receiver_account_public_key = receiver_account_info['address']
-    receiver_account_seq_num = receiver_account_info['sequence']
-
-    # Create a transaction to transfer the NFT
-    params = algod_client.suggested_params()
-    txn = algosdk.future.transaction.AssetTransferTxn(
-        creator_address=creator_account_public_key,
-        recipient_address=receiver_account_public_key,
-        close_assets_to=creator_address,
-        asset_id=asset_id,
-        amount=1,
-        params=params,
+    sp = algod_client.suggested_params()
+    # Create opt-in transaction
+    optin_txn = transaction.AssetOptInTxn(
+        sender=receiver_address, sp=sp, index=asset_id
     )
-
-    # Sign the transaction with the creator's private key
-    signed_txn = txn.sign(creator_private_key)
-
-    # Send the transaction
-    algod_client.send_transaction(signed_txn)
-
+    signed_optin_txn = optin_txn.sign(receiver_private_key)
+    txid = algod_client.send_transaction(signed_optin_txn)
     # Wait for the transaction to be confirmed
-    confirmed_txn = None
-    while confirmed_txn is None:
-        try:
-            txn_id = signed_txn.transaction.get_txid()
-            confirmed_txn = algod_client.pending_transaction_info(txn_id)
-        except Exception:
-            pass
+    results = transaction.wait_for_confirmation(algod_client, txid, 4)
 
-    # Sign the transaction with the receiver's private key to close out any remaining assets
-    close_txn = algosdk.future.transaction.AssetTransferTxn(
-        creator_address=receiver_account_public_key,
-        recipient_address=creator_account_public_key,
-        close_assets_to=receiver_address,
-        asset_id=asset_id,
-        amount=0,
-        params=params,
-    ).sign(receiver_private_key)
+    # ASSET_XFER
+    # Create transfer transaction
+    xfer_txn = transaction.AssetTransferTxn(
+        sender=creator_address,
+        sp=sp,
+        receiver=receiver_address,
+        amt=1,
+        index=asset_id,
+    )
+    signed_xfer_txn = xfer_txn.sign(creator_private_key)
+    txid = algod_client.send_transaction(signed_xfer_txn)
+    print(f"Sent transfer transaction with txid: {txid}")
 
-    # Send the close transaction
-    algod_client.send_transaction(close_txn)
-
-    return confirmed_txn['asset-transfer-transaction']['asset-id']
+    results = transaction.wait_for_confirmation(algod_client, txid, 4)
+    
